4f02953f3d2461fbc33a72e5d5f8b85ed664c8e4
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-15 07:21:54 -0300

    feat: create route GET /:id for users

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const { OK } = require('./utils/httpResponse/httpStatusCode');
-const users = require('./database/users.json');
+const express = require("express");
+const { OK } = require("./utils/httpResponse/httpStatusCode");
+const users = require("./database/users.json");
 
 const app = express();
 
-app.get('/', (_req, res) => {
-    res.status(OK).json(users)
-})
+app.get("/", (_req, res) => {
+  res.status(OK).json(users);
+});
 
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((user) => user.id === Number(id));
+  res.status(OK).json(user);
+});
 
 module.exports = app;
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,21 +1,27 @@
-
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../src/app.js');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../src/app.js");
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-const users = require('../src/database/users.json');
+const users = require("../src/database/users.json");
 
-describe('Test Users routes', function () {
-    describe('/GET', function () {
-        it('should have status 200', async function () {
-            const response = await chai.request(app)
-                .get('/')
-            expect(response).to.have.status(200)
-            expect(response.body).to.deep.equal(users);
-        })
-    })
-})
+describe("Test Users routes", function () {
+  describe("/GET", function () {
+    it("should have status 200", async function () {
+      const response = await chai.request(app).get("/");
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal(users);
+    });
+  });
+  describe("GET /:id", function () {
+    it("should return user with id 1", async function () {
+      const userId = 1;
+      const response = await chai.request(app).get(`/${userId}`);
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal(users[0]);
+    });
+  });
+});
