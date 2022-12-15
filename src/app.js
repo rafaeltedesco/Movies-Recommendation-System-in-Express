feb61c4d8343106c5b05fc574fb2f208427be48f
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-15 07:36:23 -0300

    feat: GET /:id movie by id

--- src/app.js
+++ src/app.js
@@ -19,4 +19,10 @@ app.get("/movies", (_req, res) => {
   res.status(OK).json(movies);
 });
 
+app.get("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id === Number(id));
+  res.status(OK).json(movie);
+});
+
 module.exports = app;
--- tests/movies.test.js
+++ tests/movies.test.js
@@ -11,10 +11,15 @@ const movies = require("../src/database/movies.json");
 describe("Test Movies routes", function () {
   describe("GET / movies", function () {
     it("should return all movies", async function () {
-      const response = await chai.request(app)
-        .get("/movies");
+      const response = await chai.request(app).get("/movies");
       expect(response).to.have.status(200);
       expect(response.body).to.deep.equal(movies);
+    });
+    it("should return movie with id 1", async function () {
+      const movieId = 1;
+      const response = await chai.request(app).get(`/movies/${movieId}`);
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal(movies[0]);
     });
   });
 });
