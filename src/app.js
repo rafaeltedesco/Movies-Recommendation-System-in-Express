dbda75fc4843711b7bb41f9efab28ea668a1daee
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-15 07:16:08 -0300

    refactor: add module for statusCode

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { OK } = require('./utils/httpResponse/httpStatusCode');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).json()
+    res.status(OK).json()
 })
 
 
