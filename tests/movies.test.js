b827efaa8bdb45a6ceee64ed9548643a5a062f11
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-15 07:33:57 -0300

    test: get all movies

---
+++ tests/movies.test.js
@@ -0,0 +1,20 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../src/app");
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const movies = require("../src/database/movies.json");
+
+describe("Test Movies routes", function () {
+  describe("GET / movies", function () {
+    it("should return all movies", async function () {
+      const response = await chai.request(app)
+        .get("/movies");
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal(movies);
+    });
+  });
+});
