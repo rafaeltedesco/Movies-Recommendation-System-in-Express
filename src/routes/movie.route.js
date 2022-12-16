9cfd6d715d05e80f88fda9ab140f519cb9e16629
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-16 11:41:14 -0300

    style: remove blank line

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -5,7 +5,6 @@ const movieController = require("../controllers/movie.controller");
 const router = Router();
 
 router.get("/", movieController.getAllMovies);
-
 router.get("/:id", movieController.getMovieById);
 
 module.exports = router;
