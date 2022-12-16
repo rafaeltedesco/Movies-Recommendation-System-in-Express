1d8fd09bef4f32ad57e9f410ba6c961740204939
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-16 16:55:10 -0300

    refactor: remove unused code

--- src/routes/recommendation.route.js
+++ src/routes/recommendation.route.js
@@ -4,16 +4,6 @@ const { getUsersSimilarity, sortSimilarity } = require("../services/recommendati
 
 const router = Router();
 
-// router.get('/user/:id', (req, res) => {
-//     res.status(200).json({
-//         prob: 4.52656746,
-//         movie: {
-//           id: 6,
-//           name: "Pulp Fiction",
-//         },
-//     })
-// })
-
 router.get("/show-similar-users/:userId", userExists, async (req, res) => {
     const similarUsers = await getUsersSimilarity(req.userId);
     sortSimilarity(similarUsers)
