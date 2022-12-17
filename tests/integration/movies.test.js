127dfe88e97f7f6f3d3d116889663fd81ae11f84
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-17 11:51:17 -0300

    test: mock readFile and writeFile for movies

--- tests/integration/movies.test.js
+++ tests/integration/movies.test.js
@@ -1,31 +1,68 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../../src/app");
-
+const sinon = require("sinon");
+const fs = require("fs").promises;
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-const movies = require("../../src/database/movies.json");
+const movies = JSON.stringify([
+  {
+    id: 1,
+    name: "Avatar",
+  },
+  {
+    id: 2,
+    name: "Wakanda Forever",
+  },
+  {
+    id: 3,
+    name: "Violent Night",
+  },
+  {
+    id: 4,
+    name: "The Avengers",
+  },
+  {
+    id: 5,
+    name: "Toy Story",
+  },
+  {
+    id: 6,
+    name: "Pulp Fiction",
+  },
+  {
+    id: 7,
+    name: "Jurassic Park",
+  },
+]);
+
+const parsedMovies = JSON.parse(movies);
+
 const {
   OK,
   NOT_FOUND,
 } = require("../../src/utils/httpResponse/httpStatusCode");
 
 describe("Test Movies routes", function () {
+  afterEach(sinon.restore);
   describe("GET / movies", function () {
     it("should return all movies", async function () {
+      sinon.stub(fs, "readFile").resolves(movies);
       const response = await chai.request(app).get("/movies");
       expect(response).to.have.status(OK);
-      expect(response.body).to.deep.equal(movies);
+      expect(response.body).to.deep.equal(parsedMovies);
     });
     it("should return movie with id 1", async function () {
+      sinon.stub(fs, "readFile").resolves(movies);
       const movieId = 1;
       const response = await chai.request(app).get(`/movies/${movieId}`);
       expect(response).to.have.status(OK);
-      expect(response.body).to.deep.equal(movies[0]);
+      expect(response.body).to.deep.equal(parsedMovies[0]);
     });
     it('should return 404 and message "User not found" when request for an invalid id', async function () {
+      sinon.stub(fs, "readFile").resolves(movies);
       const invalidMovieId = 10;
       const response = await chai.request(app).get(`/movies/${invalidMovieId}`);
       expect(response).to.have.status(NOT_FOUND);
@@ -35,7 +72,9 @@ describe("Test Movies routes", function () {
     });
   });
   describe("POST /movies", function () {
-    it.skip("should add a new movie", async function () {
+    it("should add a new movie", async function () {
+      sinon.stub(fs, "writeFile").resolves([{ insertedId: 8 }]);
+
       const newMovie = {
         name: "Black Adam",
       };
@@ -44,18 +83,22 @@ describe("Test Movies routes", function () {
         ...newMovie,
       };
 
+      sinon.stub(fs, "readFile").resolves(movies);
+
       let response = await chai.request(app).post("/movies").send(newMovie);
 
       expect(response).to.have.status(201);
       expect(response.body).to.deep.equal(expectedBody);
 
-      response = await chai.request(app)
-        .get('/movies')
+      sinon.restore();
 
-      
-      expect(response.body).to.deep.equal([...movies, expectedBody])
+      sinon
+        .stub(fs, "readFile")
+        .resolves(JSON.stringify([...parsedMovies, expectedBody]));
 
+      response = await chai.request(app).get("/movies");
 
+      expect(response.body).to.deep.equal([...parsedMovies, expectedBody]);
     });
   });
 });
